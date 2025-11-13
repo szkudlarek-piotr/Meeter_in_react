0d4983187c375b7ec210809a31e138a3e57d36d9
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2025-11-13 07:07:43 +0100

    Created a dropdown menu for the WeddingAddingFrontend.

--- src/assets/Components/FrontendToAddWedding.jsx
+++ src/assets/Components/FrontendToAddWedding.jsx
@@ -1,6 +1,7 @@
 import CaptionedPhoto from "./Multiuse/CaptionedPhoto.jsx";
 import SimpleTextInput from "./SimpleTextInput";
 import DatePickerWithClock from "./Multiuse/DatePickerWithClock";
+import DropdownMenu from './Multiuse/DropdownMenu.jsx'
 import { useState } from "react";
 
 export default function FrontendToAddWedding() {
@@ -9,14 +10,138 @@ export default function FrontendToAddWedding() {
     const initialPartnerData = {name: "Partnerka", photo: "https://rlv.zcache.com/anonymous_logo_square_sticker-r5e3b35e5655c45d5bc36562960a42d9b_zg2qgs_644.webp?rlvnet=1", id: "0"}
     const initialBrideData = {name: "Panna Młoda", photo: "https://rlv.zcache.com/anonymous_logo_square_sticker-r5e3b35e5655c45d5bc36562960a42d9b_zg2qgs_644.webp?rlvnet=1", id: "0"}
 
+    
+
+    const [groomInputData, setGroomInputData] = useState("")
+    const [brideInputData, setBrideInputData] = useState("")
+    const [partnerInputData, setPartnerInputData] = useState("")
+    const [groomDropdownData, setGroomDropdownData] = useState([])
+    const [brideDropdownData, setBrideDropdownData] = useState([])
+    const [partnerDropdownData, setPartnerDropdownData] = useState([])
     const [groomData, setGroomData] = useState(initialGroomData)
     const [partnerData, setPartnerData] = useState(initialPartnerData)
     const [brideData, setBrideData] = useState(initialBrideData)
+    
+    async function handleGroomInputChange(value) {
+        setGroomInputData(value);
+
+        if (value.length < 2) {
+            setGroomDropdownData([]);
+            return;
+        }
+
+        try {
+            const response = await fetch(
+                `http://localhost:3000/get-human-from-substring?substring=${encodeURIComponent(value)}`
+            );
+            if (!response.ok) throw new Error("Błąd sieci!");
+            const data = await response.json();
+            setGroomDropdownData(data);
+        } catch (error) {
+            console.error("Błąd pobierania danych:", error);
+        }
+    }
+
+    async function handleBrideInputChange(value) {
+        setBrideInputData(value);
+
+        if (value.length < 2) {
+            setBrideDropdownData([]);
+            return;
+        }
+
+        try {
+            const response = await fetch(
+                `http://localhost:3000/get-human-from-substring?substring=${encodeURIComponent(value)}`
+            );
+            if (!response.ok) throw new Error("Błąd sieci!");
+            const data = await response.json();
+            setBrideDropdownData(data);
+        } catch (error) {
+            console.error("Błąd pobierania danych:", error);
+        }
+    }
+
+
+    async function handlePartnerInputChange(value) {
+        setPartnerInputData(value);
+
+        if (value.length < 2) {
+            setPartnerDropdownData([]);
+            return;
+        }
+
+        try {
+            const response = await fetch(
+                `http://localhost:3000/get-human-from-substring?substring=${encodeURIComponent(value)}`
+            );
+            if (!response.ok) throw new Error("Błąd sieci!");
+            const data = await response.json();
+            setPartnerDropdownData(data);
+        } catch (error) {
+            console.error("Błąd pobierania danych:", error);
+        }
+    }
+
+
+    async function handleGroomChoice(person) {
+        console.log(`Wybrano pana młodego: ${person.toString()}`)
+        setGroomData(person)
+        setGroomInputData(person.name)
+        setGroomDropdownData([])
+    }
+
+    async function handleBrideChoice(person) {
+        console.log(`Wybrano partnerkę: ${person}`)
+        setBrideData(person)
+        setBrideInputData(person.name)
+        setBrideDropdownData([])
+    }
+
+
+    async function handlePartnerChoice(person) {
+        console.log(`Wybrano partnera: ${person}`)
+        setPartnerData(person)
+        setPartnerInputData(person.name)
+        setPartnerDropdownData([])
+    }
+
+
     return (
         <>
             <h3>Data wesela</h3>
             <DatePickerWithClock style={{width: "80%"}}/>
             <SimpleTextInput headerText="Nazwa wesela" placeholder="Wpisz nazwę wesela..." id="weddingNamInput" />
+
+            <h3>Pan Młody:</h3>
+            <DropdownMenu 
+                inputValue={groomInputData} 
+                placeholder="Pan Młody" 
+                choiceOptions={groomDropdownData}
+                onInputChange={handleGroomInputChange}
+                onOptionDoubleClick={handleGroomChoice}  
+            />
+
+            <h3>Panna Młoda:</h3>
+            <DropdownMenu 
+                inputValue={brideInputData} 
+                placeholder="Panna Młoda" 
+                choiceOptions={brideDropdownData}
+                onInputChange={handleBrideInputChange}
+                onOptionDoubleClick={handleBrideChoice}   
+            />
+
+
+            <h3>Partnerka:</h3>
+            <DropdownMenu 
+                inputValue={partnerInputData} 
+                placeholder="Partnerka" 
+                choiceOptions={partnerDropdownData}
+                onInputChange={handlePartnerInputChange}
+                onOptionDoubleClick={handlePartnerChoice}  
+            />
+
+
             <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-evenly"}}>
                 <CaptionedPhoto divWidth="30" photoWidth="50" photoBorderWidthInPx="2" photoCaption={groomData.name} captionWidth="80" borderRadius="20" photoAdress={groomData.photo} />
 
---
+++ src/assets/Components/Multiuse/DropdownOptionForHuman.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components"
+
+const HumanDropdownOption= styled.div`
+    height: 50px;
+    background-color: white;
+    display: flex;
+    margin-left: auto;
+    margin-right: auto;
+
+    border-bottom: 1px solid black;
+    border-right: 1px solid black;
+    border-left: 1px solid black;
+    &> img {
+        width: 40px;
+        margin-top: 5px;
+        margin-bottom: 5px;
+        height: 40px;
+        border-radius: 20px;
+        margin-left: calc(5% - 20px);
+        margin-right: calc(5% - 20px);
+    }
+    &>.humanName {
+        font-size: 30px;
+        width: 90%;
+    }
+    &:hover {
+        font-weight: 700;
+        background-color: red;
+    }
+`
+
+export default function DropdownOptionForHuman({photo, name, id, onDoubleClick}) {
+    return (
+        <HumanDropdownOption onDoubleClick={onDoubleClick} key={id}> 
+            <img src={photo} alt={name}/>
+            <div className="humanName">{name}</div>
+        </HumanDropdownOption>)
+}
