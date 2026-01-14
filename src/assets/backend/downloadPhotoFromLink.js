ecf12cb45c5fea67d1d0fc94aa27976d94ca1c4d
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2026-01-14 23:03:04 +0100

    Craeted a function for downloading photos from links.

---
+++ src/assets/backend/downloadPhotoFromLink.js
@@ -0,0 +1,33 @@
+import fetch from 'node-fetch'
+import path from 'path'
+import fs from 'fs'
+
+export default async function downloadPhotoFromLink(photoLink, folderDirectory, photoName) {
+    console.log(`Próbuję pobrać fotkę z ${photoLink}.`)
+    try {
+        const response = await fetch(photoLink, {method: "HEAD"})
+        const fileType= response.headers.get("Content-Type").split("/")[0]
+        const fileExtension = response.headers.get("Content-Type").split("/")[1]
+        if (fileType == "image") {
+            const fileName = `${photoName}.${fileExtension}`
+            const writingDir = path.join(folderDirectory, fileName)
+            const imageResponse = await fetch(photoLink)
+            if (imageResponse.ok) {
+                const arrayBuffer = await imageResponse.arrayBuffer()
+                //This way, you can accidentaly replace an old image on the disk.
+                const writingJob = await fs.writeFileSync(writingDir, Buffer.from(arrayBuffer))
+                return fileName
+            }
+            else {
+                console.log(imageResponse.status)
+            }
+        }
+        else {
+            console.log("This link does not point to an image.")
+        }
+    }
+    catch (error) {
+        console.log(error)
+        return null
+    }
+}
