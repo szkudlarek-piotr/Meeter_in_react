c9df81b7f55e8a184e62286a919e3963426ae9d9
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2026-01-18 20:48:35 +0100

    Added the "name" prop to the RadioOptionsPicker, which is necessary for having multiple pickers on a one page.

--- src/assets/Components/Multiuse/RadioOptionsPicker.jsx
+++ src/assets/Components/Multiuse/RadioOptionsPicker.jsx
@@ -2,14 +2,14 @@ import styled from "styled-components";
 
 const StyledOptionsContainer = styled.div`
     margin-top:20px;
-    margin-bottom: 20px;
+    margin-bottom: 30px;
     display:flex;
     margin-left:10%;
     margin-right: 10%;
     justify-content: space-evenly;
 `
 
-export default function RadioOptionsPicker({ header, options, chosenOptionName, onChangeFunction }) {
+export default function RadioOptionsPicker({ header, options, chosenOptionName, onChangeFunction, name="defaultPickerNAme" }) {
   return (
     <>
       <h2>{header}</h2>
@@ -20,8 +20,9 @@ export default function RadioOptionsPicker({ header, options, chosenOptionName, 
               type="radio"
               name="photoSource"
               value={option.value}
-              checked={chosenOptionName === option.value}
+              checked={chosenOptionName == option.value}
               onChange={onChangeFunction}
+              name={name}
             />
             {option.text}
           </label>
