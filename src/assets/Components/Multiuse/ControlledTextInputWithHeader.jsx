51131ca7b0acdfcede82a3c1284ae98712fefa72
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2026-01-18 20:51:21 +0100

    Included the ControlledTextInput component in the ControledTextInputWithHeader component.

--- src/assets/Components/Multiuse/ControlledTextInputWithHeader.jsx
+++ src/assets/Components/Multiuse/ControlledTextInputWithHeader.jsx
@@ -1,13 +1,11 @@
 import styled from 'styled-components'
 import ControlledTextInput from './ControlledTextInput'
 
-
-
 export default function ControledTextInputWithHeader({headerText, placeholderValue, fieldValue, changeFieldValueFunction}) {
     return (
     <div>
         <h2>{headerText}</h2>
-        <input style={{height: "30px", minWidth: "100px"}} type="text" value={fieldValue} placeholder={placeholderValue} onChange={(e) => changeFieldValueFunction(e.target.value)}/>
+        <ControlledTextInput style={{height: "30px", minWidth: "100px"}} type="text" value={fieldValue} placeholderValue={placeholderValue} onChange={(e) => changeFieldValueFunction(e.target.value)}/>
     </div>
     )
 }
