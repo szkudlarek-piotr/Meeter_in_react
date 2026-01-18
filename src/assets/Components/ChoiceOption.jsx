bcddf59940c09fa57d22cd5df1f9f97c10ddaaa2
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2026-01-18 20:47:30 +0100

    Changed the styling of the ChoiceOption modal.

--- src/assets/Components/ChoiceOption.jsx
+++ src/assets/Components/ChoiceOption.jsx
@@ -3,12 +3,15 @@ import styled from "styled-components";
 const MenuOption = styled.a`
     font-size: 20px;
     font-family: Times New Roman;
-    height:30px;
+    height: 30px;
+    text-align: center;
+
+    
 
     &:hover {
         font-weight: 700;
     }
 `
-export function ChoiceOption({children, id, onClick}){
-    return <MenuOption id={id} onClick={onClick} key={id}>{children}</MenuOption>
+export default function ChoiceOption({children, id, onClick}){
+    return <MenuOption id={id} onClick={onClick}>{children}</MenuOption>
 }
