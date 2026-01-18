93735096c849336b8956560dafcb5f71666ff7d3
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2026-01-18 20:52:47 +0100

    Removed file for dropdown options.

--- src/assets/Components/DropdownOption.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function DropdownOptionForHuman({photo, name, id}) {
-    return (
-        <div onDoubleClick={}>
-            <img src={photo} alt={name}/>
-            <div className="humanName">{name}</div>
-        </div>)
-}
