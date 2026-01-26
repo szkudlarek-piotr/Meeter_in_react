fc1d5c5a4d8a21c8004ed2c63ebf4fbb999cc873
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2026-01-26 18:32:17 +0100

    Addided trips the the getCalendar function and included adding eventPhotos into the function.

--- backend/getCalendar.js
+++ backend/getCalendar.js
@@ -3,6 +3,10 @@ import dotenv from 'dotenv'
 import path from 'path'
 import fs from 'fs'
 import { fileURLToPath } from "url"
+import getEventPhotoFromId from './getEventPhotoFromId.js'
+import getEventPhotoFromGenericPhoto from './getEventPhotoFromGenericPhoto.js'
+
+
 dotenv.config()
 
 const pool = mysql.createPool({
@@ -17,13 +21,12 @@ const __dirname = path.dirname(__filename)
 function getHumanPhotoUrl(humanId) {
     const photosDir = path.join(__dirname, "photos")
     const photoPath = path.join(photosDir, `${humanId}.jpg`)
-    const defaultPhoto = "	http://localhost:3000/human-photo/anonymous.jpg"
+    const defaultPhoto = "http://localhost:3000/human-photo/anonymous.jpg"
 
     return fs.existsSync(photoPath) ? `http://localhost:3000/human-photo/${humanId}.jpg`: defaultPhoto
 }
 
 export default async function getCalendar(year) {
-    //console.log("Uruchamiam wyciągaczkę kalendarzową.")
 
     const daysObjects = {}
 
@@ -122,31 +125,78 @@ export default async function getCalendar(year) {
 
 
     const eventsData = `
-        SELECT e.id, nameOfEvent AS shortDesc, meComingDate, meLeavingDate, Generic_photo, ec.human_id AS humanId
+        SELECT e.id AS id, nameOfEvent AS shortDesc, meComingDate, meLeavingDate, Generic_photo AS genericPhoto, ec.human_id AS humanId
         FROM event_companion ec
         JOIN events e ON e.id = ec.event_id
         WHERE YEAR(meComingDate) = ? OR YEAR(meLeavingDate) = ?
     `
     const [eventsDataReq] = await pool.query(eventsData, [year, year])
-    //console.log(eventsDataReq)
     for (let calendarEvent of eventsDataReq) {
         const meComingDate = new Date(calendarEvent.meComingDate)
         const meLeavingDate = new Date(calendarEvent.meLeavingDate)
         const humanPhotoUrl = getHumanPhotoUrl(calendarEvent.humanId)
         const shortDesc = calendarEvent.shortDesc
+        let eventPhotoUrl
+        if (calendarEvent.genericPhoto.length > 2) {
+            eventPhotoUrl = getEventPhotoFromGenericPhoto(calendarEvent.genericPhoto)
+        }
+        else {
+            eventPhotoUrl = getEventPhotoFromId(calendarEvent.id)
+        }
         for (let checkedDate=meComingDate; checkedDate<=meLeavingDate; checkedDate.setDate(checkedDate.getDate() + 1)) {
             const dayString = checkedDate.toLocaleDateString('sv-SE')
-            //console.log(dayString)
             if (daysObjects[dayString]["class"] === "") {
                 daysObjects[dayString] = {
                     "class": "event",
                     "interactionsDict": {[meComingDate.toLocaleDateString('sv-SE')]: shortDesc},
-                    "photos": [humanPhotoUrl]
+                    "photos": [humanPhotoUrl, eventPhotoUrl]
                 }
             }
             else {
                 if (!daysObjects[dayString]["class"].includes("event")) {
                     daysObjects[dayString]["class"] = daysObjects[dayString]["class"] + "_event"
+                }
+
+                if (!daysObjects[dayString]["photos"].includes(humanPhotoUrl)) {
+                    daysObjects[dayString]["photos"].push(humanPhotoUrl)
+                }
+
+                if (!daysObjects[dayString]["photos"].includes(eventPhotoUrl)) {
+                    daysObjects[dayString]["photos"].push(eventPhotoUrl)
+                }
+
+                if (!Object.values(daysObjects[dayString]["interactionsDict"]).includes(shortDesc)) {
+                    daysObjects[dayString]["interactionsDict"] = shortDesc
+                }
+            }
+        }
+    }
+
+
+    const tripsReqText = `
+        SELECT c.Date_start AS dateStart, c.Date_stop AS dateStop, c.Place AS shortDesc, cc.human_id AS humanId
+        FROM citybreaks c
+        JOIN citybreak_companion cc ON c.ID = cc.citybreak_id
+        WHERE YEAR(c.Date_start) = ? OR YEAR(c.Date_stop) = ?;
+    `
+    const [tripsReq] = await pool.query(tripsReqText, [year, year])
+    for (let record of tripsReq) {
+        const dateStart = new Date(record.dateStart)
+        const dateStop = new Date(record.dateStop)
+        const shortDesc = record.shortDesc
+        const humanPhotoUrl = getHumanPhotoUrl(record.humanId)
+        for (let checkedDate=dateStart; checkedDate<=dateStop; checkedDate.setDate(checkedDate.getDate() + 1)) {
+            const dayString = checkedDate.toLocaleDateString('sv-SE')
+            if (daysObjects[dayString]["class"] === "") {
+                daysObjects[dayString] = {
+                    "class": "trip",
+                    "interactionsDict": {[dateStart.toLocaleDateString('sv-SE')]: shortDesc},
+                    "photos": [humanPhotoUrl]
+                }
+            }
+            else {
+                if (!daysObjects[dayString]["class"].includes("trip")) {
+                    daysObjects[dayString]["class"] = daysObjects[dayString]["class"] + "_trip"
                 }
                 if (!daysObjects[dayString]["photos"].includes(humanPhotoUrl)) {
                     daysObjects[dayString]["photos"].push(humanPhotoUrl)
@@ -157,6 +207,7 @@ export default async function getCalendar(year) {
             }
         }
     }
+
     
         for (let dateIdentifier in daysObjects) {
         if (daysObjects[dateIdentifier]["class"] != "wedding") {
@@ -191,9 +242,5 @@ export default async function getCalendar(year) {
             daysObjects[dateIdentifier]["computedTitle"] = daysObjects[dateIdentifier]["title"]
         }
     }
-
-
-
-    
     return daysObjects
 }
