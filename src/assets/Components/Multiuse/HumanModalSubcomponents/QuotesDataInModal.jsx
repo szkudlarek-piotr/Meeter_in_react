39e8320e8b2d6bdcad34cf729c373f1c5fa3d081
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2026-01-29 21:09:11 +0100

    Modified the component for displaying quotes in the HumanModal - if the aded uote is public, it will be instantly shown.

--- src/assets/Components/Multiuse/HumanModalSubcomponents/QuotesDataInModal.jsx
+++ src/assets/Components/Multiuse/HumanModalSubcomponents/QuotesDataInModal.jsx
@@ -3,7 +3,6 @@ import sanitizeHtml from 'sanitize-html';
 import { useEffect, useState } from "react";
 import ControlledTextArea from '../SimpleControlledComponents/ControlledTextArea.jsx'
 import RadioOptionsPicker from '../RadioOptionsPicker.jsx'
-import InputForAddingUotedFOrPredefinedPerson from "./InputForAddingQuoteForPredefinedPerson";
 
 const StyledButton = styled.button`
     width: 50%;
@@ -26,6 +25,7 @@ const StyledTextContainer = styled.div`
   font-family: cursive;
   padding: 0 5%;
   overflow-y: auto;
+
   flex: 1;
   min-height: 0; 
   display: flex;
@@ -35,7 +35,6 @@ const StyledTextContainer = styled.div`
 
 
 const StyledQuoteInQuotesList = styled.div`
-  margin: 22px 12%;
   padding: 6px 0;
   width: 90%;
 
@@ -52,10 +51,8 @@ const StyledQuoteInQuotesList = styled.div`
 
 
 export default function QuotesDataInModal({humanId, headerText}) {
-  console.log(humanId)
   const [quoteData, setQuoteData] = useState([])
   const [quoteInserterState, setQuoteInserterState] = useState(setInitialState())
-  const [isAdding, setIsAdding] = useState(false)
 
 
 
@@ -73,7 +70,6 @@ export default function QuotesDataInModal({humanId, headerText}) {
         }))
     }
 
-    useEffect(() => {
 
     const getHumanQuotes = async () => {
       const fetchResult = await fetch(
@@ -84,39 +80,39 @@ export default function QuotesDataInModal({humanId, headerText}) {
       setQuoteData(quotesJson)
     }
 
-    getHumanQuotes()
-  },[])
+
+    useEffect(() => {
+      getHumanQuotes()
+    }, [quoteData])
+
+
+
 
 
     function setQuotePrivacy(e) {
         setSingleProperty("isPublic", e.target.value)
     }
 
-
-          useEffect(() =>{
-            if (isAdding == false ) return;
-            const addQuoteFunction = async () => {
-                const addQuoteReq = await fetch(`http://localhost:3000/add-quote`, 
-                    {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify({
-                            humanId: humanId,
-                            quote: quoteInserterState.quoteValue,
-                            isPublic: quoteInserterState.isPublic
-                        })
-                    }
-                )
-                const addQuoteJson = await addQuoteReq.json()
-                if (addQuoteReq.ok) {
-                    setQuoteInserterState(setInitialState())
-                    //setInsertResult({message: "Pomyślnie dodano cytat!", status: "1"})
-                }
-            }
-            addQuoteFunction()
-        }, [isAdding])
+    const addHumanQuote = async () => {
+      const addQuoteReq = await fetch(`http://localhost:3000/add-quote`, 
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          humanId: humanId,
+          quote: quoteInserterState.quoteValue,
+          isPublic: quoteInserterState.isPublic
+        })
+        }
+      )
+      const addQuoteJson = await addQuoteReq.json()
+      if (addQuoteReq.ok && quoteInserterState.isPublic == "1") {
+        getHumanQuotes()
+      }
+      setQuoteInserterState(setInitialState())
+    }
     
   
 
@@ -131,14 +127,15 @@ export default function QuotesDataInModal({humanId, headerText}) {
 
   return (
       <>
-        <h2 style={{fontSize: "36px", fontWeight: "700"}}>{headerText}</h2>
+        
         
         <StyledTextContainer>
+          <h2 style={{fontSize: "36px", fontWeight: "700"}}>{headerText}</h2>
           {mappedQuotes}
           <h2>Dodaj nowy cytat</h2>
-            <ControlledTextArea id="quoteInputForGivenPerson" height="200px" placeholderValue="Wpisz nowy cytat!" fieldValue={quoteInserterState.quoteValue} changeFieldValue={(v) =>setQuoteInserterState(prev => ({ ...prev, quoteValue: v }))} />
+          <ControlledTextArea style={{marginLeft: "auto", marginRight: "auto"}} id="quoteInputForGivenPerson" height="200px" placeholderValue="Wpisz nowy cytat!" fieldValue={quoteInserterState.quoteValue} changeFieldValue={(v) =>setQuoteInserterState(prev => ({ ...prev, quoteValue: v }))} />
           <RadioOptionsPicker header="Czy cytat ma być publiczny?" options={[ { value: "1", text: "Tak" }, { value: "0", text: "Nie" }]} chosenOptionName={quoteInserterState.isPublic} onChangeFunction={setQuotePrivacy} /> <br/>          
-          <StyledButton onClick={() => {setIsAdding(true)}}>Dodaj cytat</StyledButton>
+          <StyledButton onClick={() => {addHumanQuote()}}>Dodaj cytat</StyledButton>
         </StyledTextContainer>
         
       </>
