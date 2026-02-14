53689a2aa1652d277ff5106e440f3d2175369480
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2026-02-14 08:22:12 +0100

    CHanged the width of the ContentBox in the HumanModal.

--- src/assets/Components/Multiuse/HumanModal.jsx
+++ src/assets/Components/Multiuse/HumanModal.jsx
@@ -39,7 +39,7 @@ const MenuBox = styled.div`
 `
 const ContentBox = styled.div`
     height: 100%;
-    width: 80%;
+    width: 100%;
     text-align: center;
     display: flex;
     flex-direction: column;
